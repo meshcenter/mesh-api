28345f7d860107841ce5f75486c9c4d576fdff4d
olivernyc <oliver@static.network>
2019-11-05 17:58:59 -0500

    Load env variables

--- webpack.functions.js
+++ webpack.functions.js
@@ -1,6 +1,8 @@
 const path = require("path");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
+	plugins: [new Dotenv()],
 	resolve: {
 		alias: {
 			"pg-native": path.join(__dirname, "src/aliases/pg-native.js"),
