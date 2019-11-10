592d6b2e7d393271bf470385b50f1bd73c480f12
olivernyc <oliver@static.network>
2019-11-09 20:03:37 -0500

    Include node devices

--- src/los.js
+++ src/los.js
@@ -212,12 +212,18 @@ export async function handler(event, context) {
 		return performQuery(
 			`SELECT
 				nodes.*,
-				buildings.bin
+				buildings.bin,
+				json_agg(json_build_object('id', devices.id, 'type', device_types, 'lat', devices.lat, 'lng', devices.lng, 'alt', devices.alt, 'azimuth', devices.azimuth, 'status', devices.status, 'name', devices.name, 'ssid', devices.ssid, 'notes', devices.notes, 'create_date', devices.create_date, 'abandon_date', devices.abandon_date)) AS devices
 			FROM
 				nodes
 				JOIN buildings ON buildings.id = nodes.building_id
+				LEFT JOIN devices ON nodes.id = devices.node_id
+				LEFT JOIN device_types ON device_types.id IN (devices.device_type_id)
 			WHERE
-				buildings.bin = ANY ($1)`,
+				buildings.bin = ANY ($1)
+			GROUP BY
+				nodes.id,
+				buildings.id`,
 			[bins]
 		);
 	}
