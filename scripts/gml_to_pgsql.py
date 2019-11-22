4a5a438b06284f3ae68d9a3adaf173c5fb94bf9e
olivernyc <oliver@static.network>
2019-11-22 16:11:01 -0500

    Fixes

---
+++ scripts/gml_to_pgsql.py
@@ -0,0 +1,111 @@
+# coding: utf-8
+
+# Forked from: https://github.com/Oslandia/citygml2pgsql
+
+"""Convert CityGML buildings to PostreSQL statements for insertion in table
+Before running the script, ensure that your database contains the right table.
+To create the table, run:
+    CREATE TABLE ny(gid SERIAL PRIMARY KEY, bldg_id varchar(255), bldg_bin varchar(255), geom GEOMETRY('MULTIPOLYGONZ', 2263))
+
+USAGE
+    gml_to_pgsql file1.gml table_name
+"""
+
+import os
+import sys
+import re
+from lxml import etree
+
+def linear_ring_to_wkt(ring):
+    '''
+    Convert a linear ring object with vertices in 3 dimensions to well-known text.
+    The linear ring is defined in GML by a list of space-delimited 64-bit integers for
+    each of the vertices of the ring
+    '''
+
+    dim = 3 # x, y, z
+    raw_coord = ring[0].text.split()
+
+    # Split coordinates
+    coord = [raw_coord[i:i+dim] for i in range(0, len(raw_coord), dim)]
+
+    # If ring isn't closed, close it
+    if coord[0] != coord[-1]:
+        coord.append(coord[0])
+
+    # Catch degenerate rings
+    if len(coord) < 4:
+        sys.stderr.write( 'degenerated LinearRing gml:id="'+\
+                ring.get("{http://www.opengis.net/gml}id")+'"\n')
+        return None
+
+    # Return a list of x, y, and z coordinates that together compose the LinearRing
+    # in string format
+    return "(" + ",".join([" ".join(c) for c in coord]) + ")"
+
+def polygon_to_wkt(poly):
+    '''
+    Convert a polygon composed of multiple linear rings to well-known text
+    '''
+    all_rings = [linear_ring_to_wkt(ring) for ring in poly.iter(insert_namespace('LinearRing', poly)) ]
+
+    # remove degenerate rings (i.e. those that have fewer than 4 vertices)
+    sanitized = filter(None, all_rings)
+
+    if not sanitized:
+        sys.stderr.write('degenerate polygon gml:id="{}'.format(poly.get("{http://www.opengis.net/gml}id")))
+        return None
+
+    return "({})".format(",".join(sanitized))
+
+def insert_namespace(target, root):
+    '''
+    Given a target string, iterate through the XML tree until we find a matching tag
+    and prepend the tag's namespace. This gives us a full tag name we can pass to
+    etree.iter()
+    '''
+    for e in root.iter():
+        if e.tag is etree.Comment:
+            continue
+        m = re.match(r"(.*)"+target, e.tag) if e.tag else None
+        if m:
+            return m.groups()[0]+target
+    return None
+
+# def run_psql(filename, table_name, srid, lod, geometry_column="geom", building_id_column="building_id"):
+def run_psql(filename, table_name):
+    '''
+    Iterate over a list of Building objects in a CityGML file, isolate just the RoofSurface polygons,
+    and insert them into a PostgreSQL table
+    '''
+    if not os.path.exists(filename):
+        raise RuntimeError("error: cannot find "+filename)
+
+    root = etree.parse(filename)
+
+    # Loop over the buildings
+    for building in root.iter(insert_namespace('Building', root)):
+        bldg_id = building.get("{http://www.opengis.net/gml}id")
+        bldg_bin = ""
+
+        # Loop over string attributes to get BIN
+        for attribute in building.iter("{http://www.opengis.net/citygml/generics/1.0}stringAttribute"):
+            if attribute.attrib.get("name") == "BIN":
+                bldg_bin = attribute[0].text
+
+        # Get the polygons for this building
+        polys = [polygon_to_wkt(p) for p in building.iter(insert_namespace('Polygon', building))]
+
+        if polys != None:
+            sql = "INSERT INTO {} (geom, bldg_id, bldg_bin) VALUES ('SRID=2263; MULTIPOLYGON({})'::geometry, '{}', '{}');".format(
+                table_name, ','.join(polys), bldg_id, bldg_bin)
+            print(sql)
+        else:
+            sys.stderr.write( 'degenerate building geometry gml:id={}'.format(bldg_id))
+
+if __name__ == '__main__':
+    gml = sys.argv[1]
+    table_name = sys.argv[2]
+    sys.stderr.write("converting {}\n".format(gml))
+    run_psql(sys.argv[1], sys.argv[2])
+
--- src/kml/nodes.js
+++ src/kml/nodes.js
@@ -1,80 +1,39 @@
 import { performQuery } from "../db";
 
-const stylesKml = `<Style id="hubLink">
-	<LineStyle>
-		<color>FF00FFFF</color>
-		<width>3</width>
-	</LineStyle>
-	<PolyStyle>
-		<color>00000000</color>
-	</PolyStyle>
-</Style>
-<Style id="backboneLink">
-	<LineStyle>
-		<color>FF00FFFF</color>
-		<width>3</width>
-	</LineStyle>
-	<PolyStyle>
-		<color>00000000</color>
-	</PolyStyle>
-</Style>
-<Style id="activeLink">
-	<LineStyle>
-		<color>aa0000ff</color>
-		<width>2</width>
-	</LineStyle>
-	<PolyStyle>
-		<color>00000000</color>
-	</PolyStyle>
-</Style>
-<Style id="supernode">
-    <IconStyle>
-        <scale>0.6</scale> 
-    	<Icon>
-    		<href>https://i.imgur.com/flgK1j1.png</href>
-    	</Icon>
-        <hotSpot xunits="fraction" yunits="fraction" x="0.5" y="0.5"></hotSpot>
-    </IconStyle>
-    <LabelStyle>
-    	<scale>0</scale>
-	</LabelStyle>
-</Style>
-<Style id="hub">
-    <IconStyle>
-    	<scale>0.6</scale> 
-    	<Icon>
-    		<href>https://i.imgur.com/xbfOy3Q.png</href>
-    	</Icon>
-        <hotSpot xunits="fraction" yunits="fraction" x="0.5" y="0.5"></hotSpot>
-    </IconStyle>
-    <LabelStyle>
-    	<scale>0</scale>
-	</LabelStyle>
-</Style>
-<Style id="omni">
-    <IconStyle>
-    	<scale>0.4</scale> 
-    	<Icon>
-    		<href>https://i.imgur.com/7dMidbX.png</href>
-    	</Icon>
-        <hotSpot xunits="fraction" yunits="fraction" x="0.5" y="0.5"></hotSpot>
-    </IconStyle>
-    <LabelStyle>
-    	<scale>0</scale>
-	</LabelStyle>
-</Style>
-<Style id="node">
-    <IconStyle>
-    	<scale>0.4</scale> 
-    	<Icon>
-    		<href>https://i.imgur.com/7SIgB7Z.png</href>
-    	</Icon>
-        <hotSpot xunits="fraction" yunits="fraction" x="0.5" y="0.5"></hotSpot>
-    </IconStyle>
-    <LabelStyle>
-    	<scale>0</scale>
-	</LabelStyle>
-</Style>`;
+const getNodesQuery = `SELECT
+	nodes.*,
+	buildings.address,
+	json_agg(DISTINCT devices) as devices,
+	json_agg(DISTINCT device_types) as device_types,
+	json_agg(DISTINCT panoramas) as panoramas
+FROM
+	nodes
+	LEFT JOIN buildings ON nodes.building_id = buildings.id
+	LEFT JOIN devices ON nodes.id = devices.node_id
+	LEFT JOIN device_types ON device_types.id IN (devices.device_type_id)
+	LEFT JOIN requests ON requests.building_id = buildings.id
+	LEFT JOIN panoramas ON panoramas.request_id = requests.id
+GROUP BY
+	nodes.id,
+	buildings.id
+ORDER BY
+	nodes.create_date DESC`;
+
+const getLinksQuery = `SELECT
+	links.*,
+	json_agg(DISTINCT nodes) as nodes,
+	json_agg(DISTINCT devices) as devices,
+	json_agg(device_types) as device_types
+FROM
+	links
+	JOIN devices ON devices.id = links.device_a_id
+		OR devices.id = links.device_b_id
+	JOIN device_types ON device_types.id = devices.device_type_id
+	JOIN nodes ON nodes.id = devices.node_id
+WHERE
+	links.status = 'active'
+GROUP BY
+	links.id`;
 
 export async function getNodesKML() {
 	const nodes = await getNodes();
@@ -107,7 +66,14 @@ export async function getNodesKML() {
 	const kml = `<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
 	<Document>
-		${stylesKml}
+		${linkStyle("hubLink", "FF00FFFF", 3)}
+		${linkStyle("backboneLink", "ff00ffff", 3)}
+		${linkStyle("activeLink", "aa0000ff", 3)}
+		${nodeStyle("supernode", 0.6, "https://i.imgur.com/flgK1j1.png")}
+		${nodeStyle("hub", 0.6, "https://i.imgur.com/xbfOy3Q.png")}
+		${nodeStyle("hub", 0.6, "https://i.imgur.com/xbfOy3Q.png")}
+		${nodeStyle("omni", 0.4, "https://i.imgur.com/7dMidbX.png")}
+		${nodeStyle("node", 0.4, "https://i.imgur.com/7SIgB7Z.png")}
         ${nodesKml}
 	</Document>
 </kml>`;
@@ -136,13 +102,13 @@ function nodePlacemark(node) {
 			            <value>${node.address.replace(/&/g, "+")}</value>
 			        </Data>
 			        <Data name="devices">
-			            <value>${node.devices.map(d => d.type.name).join(", ")}</value>
+			            <value>${node.device_types.map(d => d.name).join(", ")}</value>
 			        </Data>
 			        <Data name="installed">
 			            <value>${node.create_date.toDateString()}</value>
 			        </Data>
 			        ${(node.panoramas || [])
-						.filter(p => p.url)
+						.filter(p => p)
 						.map(
 							(panorama, index) =>
 								`<Data name="panorama ${index + 1}">
@@ -154,61 +120,81 @@ function nodePlacemark(node) {
 			        <altitudeMode>absolute</altitudeMode>
 			        <coordinates>${node.lng},${node.lat},${node.alt}</coordinates>
 			    </Point>
-			    <styleUrl>${getStyle(node)}</styleUrl>
+			    <styleUrl>${nodeStyleId(node)}</styleUrl>
 			</Placemark>`;
 }
 
 function linkPlacemark(link) {
 	const [node_a, node_b] = link.nodes;
-	const [device_type_a, device_type_b] = link.device_types;
+	const coordinates = `${node_a.lng},${node_a.lat},${node_a.alt} ${node_b.lng},${node_b.lat},${node_b.alt}`;
 	return `<Placemark>
-	            <name>${node_a.id} - ${node_b.id}</name>
-	            <ExtendedData>
-	                <Data name="status">
-	                    <value>${link.status}</value>
-	                </Data>
-	                <Data name="from">
-	                    <value>${node_a.name || node_a.id}</value>
-	                </Data>
-	                <Data name="to">
-	                    <value>${node_b.name || node_b.id}</value>
-	                </Data>
-	            </ExtendedData>
-	            <LineString>
-	                <altitudeMode>absolute</altitudeMode>
-	                <coordinates>${node_a.lng},${node_a.lat},${node_a.alt} ${
-		node_b.lng
-	},${node_b.lat},${node_b.alt}</coordinates>
-	            </LineString>
-	            <styleUrl>${getLinkStyle(link)}</styleUrl>
-	        </Placemark>
-			`;
+    <name>${node_a.id} - ${node_b.id}</name>
+    <ExtendedData>
+        <Data name="status">
+            <value>${link.status}</value>
+        </Data>
+        <Data name="from">
+            <value>${node_a.name || node_a.id}</value>
+        </Data>
+        <Data name="to">
+            <value>${node_b.name || node_b.id}</value>
+        </Data>
+    </ExtendedData>
+    <LineString>
+        <altitudeMode>absolute</altitudeMode>
+        <coordinates>${coordinates}</coordinates>
+    </LineString>
+    <styleUrl>${linkStyleId(link)}</styleUrl>
+</Placemark>
+`;
 }
 
-function getStyle(node) {
-	const { name, notes, devices } = node;
+function linkStyle(id, color, width) {
+	return `<Style id="${id}">
+	<LineStyle>
+		<color>${color}</color>
+		<width>${width}</width>
+	</LineStyle>
+	<PolyStyle>
+		<color>00000000</color>
+	</PolyStyle>
+</Style>`;
+}
 
-	if (name && name.includes("Supernode")) return "#supernode";
+function nodeStyle(id, scale, icon) {
+	return `<Style id="${id}">
+    <IconStyle>
+        <scale>${scale}</scale> 
+    	<Icon>
+    		<href>${icon}</href>
+    	</Icon>
+        <hotSpot xunits="fraction" yunits="fraction" x="0.5" y="0.5"></hotSpot>
+    </IconStyle>
+    <LabelStyle>
+    	<scale>0</scale>
+	</LabelStyle>
+</Style>`;
+}
 
-	if (notes && notes.includes("hub")) return "#hub";
+const isOmni = device_type => device_type.name === "Omni";
+const isSupernode = node => node.name && node.name.includes("Supernode");
+const isHub = node => node.notes && node.notes.includes("hub");
+const isBackbone = (node, device_type) =>
+	isSupernode(node) || isHub(node) || isOmni(device_type);
 
-	if (devices.filter(device => device.type.name === "Omni").length)
-		return "#omni";
-
+function nodeStyleId(node) {
+	const { name, notes, device_types } = node;
+	if (isSupernode(node)) return "#supernode";
+	if (isHub(node)) return "#hub";
+	if (device_types.filter(isOmni).length) return "#omni";
 	return "#node";
 }
 
-function getLinkStyle(link) {
+// TODO: Need to check all devices on each node to determine color.
+function linkStyleId(link) {
 	const { nodes, device_types } = link;
 	const [node1, node2] = nodes;
 	const [device_type1, device_type2] = device_types;
-
-	const isSupernode = node => node.name && node.name.includes("Supernode");
-	const isHub = node => node.notes && node.notes.includes("hub");
-	const isOmni = device_type => device_type.name === "Omni";
-	const isBackbone = (node, device_type) =>
-		isSupernode(node) || isHub(node) || isOmni(node);
-
 	if (isHub(node1) && isHub(node2)) return "#hubLink";
 	if (isBackbone(node1, device_type1) && isBackbone(node2, device_type2))
 		return "#backboneLink";
@@ -216,43 +202,9 @@ function getLinkStyle(link) {
 }
 
 async function getNodes() {
-	return performQuery(
-		`SELECT
-			nodes.*,
-			buildings.address AS address,
-			json_agg(json_build_object('id', devices.id, 'type', device_types, 'lat', devices.lat, 'lng', devices.lng, 'alt', devices.alt, 'azimuth', devices.azimuth, 'status', devices.status, 'name', devices.name, 'ssid', devices.ssid, 'notes', devices.notes, 'create_date', devices.create_date, 'abandon_date', devices.abandon_date)) AS devices,
-			json_agg(json_build_object('id', panoramas.id, 'url', panoramas.url, 'date', panoramas.date)) AS panoramas
-		FROM
-			nodes
-			LEFT JOIN buildings ON nodes.building_id = buildings.id
-			LEFT JOIN devices ON nodes.id = devices.node_id
-			LEFT JOIN device_types ON device_types.id IN(devices.device_type_id)
-			LEFT JOIN requests ON requests.building_id = buildings.id
-			LEFT JOIN panoramas ON panoramas.request_id = requests.id
-		GROUP BY
-			nodes.id,
-			buildings.id
-		ORDER BY
-			nodes.create_date DESC`
-	);
+	return performQuery(getNodesQuery);
 }
 
 async function getLinks() {
-	return performQuery(
-		`SELECT
-			links.*,
-			json_agg(devices) as devices,
-			json_agg(device_types) as device_types,
-			json_agg(nodes) as nodes
-		FROM
-			links
-			JOIN devices ON devices.id = links.device_a_id
-				OR devices.id = links.device_b_id
-			JOIN device_types ON device_types.id = devices.device_type_id
-			JOIN nodes ON nodes.id = devices.node_id
-		WHERE
-			links.status = 'active'
-		GROUP BY
-			links.id`
-	);
+	return performQuery(getLinksQuery);
 }
