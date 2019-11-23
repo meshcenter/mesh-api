72964960ccbd49f328e9f3a48e63a674eb90cc91
Oliver Fisher <oliver@static.network>
2019-11-22 20:46:23 -0500

    Update README (#8)
    * Update readme
    
    * WIP

--- src/db/panos.js
+++ src/db/panos.js
@@ -5,11 +5,12 @@ const insertPanoQuery =
 	"INSERT INTO panoramas (url, date, request_id) VALUES ($1, $2, $3) RETURNING id";
 
 const s3 = new AWS.S3({
-	endpoint: process.env.SPACES_ENDPOINT,
-	accessKeyId: process.env.SPACES_ID,
-	secretAccessKey: process.env.SPACES_KEY
+	endpoint: process.env.S3_ENDPOINT,
+	accessKeyId: process.env.S3_ID,
+	secretAccessKey: process.env.S3_KEY
 });
 
+// TODO: Restrict this somehow. Maybe require a secret?
 export async function savePano(requestId, panoURL) {
 	if (!requestId || !panoURL) throw new Error("Bad params");
 	const values = [panoURL, new Date(), requestId];
@@ -20,7 +21,7 @@ export async function savePano(requestId, panoURL) {
 export async function getUploadURL(name, type) {
 	if (!name || !type) throw new Error("Bad params");
 	const url = await s3.getSignedUrl("putObject", {
-		Bucket: process.env.SPACES_BUCKET,
+		Bucket: process.env.S3_BUCKET,
 		Key: name,
 		ContentType: type,
 		ACL: "public-read"
