ddcb84e0aa3ca8c089460918828fb1c8803a8ee6
Aaron Greenberg <p@aaronjgreenberg.com>
2020-10-31 12:52:21 -0700

    Simplify unit-testing strategy for Slack module

--- src/slack/index.spec.js
+++ src/slack/index.spec.js
@@ -1,46 +1,27 @@
-import { WebClient } from "@slack/web-api";
 import { panoMessage } from '.';
 
-const mockPostMessage = jest.fn('slack.chat.postMessage');
-const mockUpdate = jest.fn('slack.chat.update');
-
-jest.mock("@slack/web-api", () => {
-	return {
-		WebClient: jest.fn().mockImplementation(() => {
-			return {
-				chat: {
-					postMessage: mockPostMessage,
-					update: mockUpdate,
-				},
-				conversations: {
-					list: () => {
-						return [
-							{ name: 'join-requests-test', id: 4 },
-							{ name: 'panoramas-test', id: 5 },
-							{ name: 'install-team-test', id: 6 },
-						];
-					},
-				},
-			}
-		}),
-	};
-});
-
-beforeEach(() => {
-	mockPostMessage.mockClear();
-	mockUpdate.mockClear();
-	mockWebClient.mockClear();
-});
-
 describe('panoMessage', () => {
-	it('sends a message to the panoramas channel', () => {
+	it('sends a message to the panoramas channel', async () => {
+		const slackClient = mockSlackClient();
+		slackClient.getChannel.mockResolvedValue({ name: "panoramas-test", id: 2 });
+
 		const url = 'https://example.com';
 		const nodeID = 1;
 
-		panoMessage({ url, node_id: nodeID });
+		await panoMessage(slackClient, { url, node_id: nodeID });
 
-		expect(mockPostMessage).toHaveBeenCalledTimes(1);
-		const arg = mockPostMessage.mock.calls[0][0];
-		expect(arg.channel).toBe(5)
+		expect(slackClient.postMessage).toHaveBeenCalled();
+		const { channel, text, blocks } = slackClient.postMessage.mock.calls[0][0];
+		expect(channel).toBe(2);
+		expect(text).toBe('New pano for 1!');
+		expect(blocks[0].image_url).toBe('https://example.com');
 	})
 });
+
+function mockSlackClient() {
+	return {
+		getChannel: jest.fn(),
+		postMessage: jest.fn(),
+		update: jest.fn(),
+	}
+}
