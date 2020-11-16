d0d60374d1d7f728250b7648ea9511e36da59cc9
Aaron Greenberg <p@aaronjgreenberg.com>
2020-11-15 21:51:13 -0800

    fixup! Add unit tests for kml module

--- src/kml/appointments.spec.js
+++ src/kml/appointments.spec.js
@@ -5,9 +5,38 @@ import { getAppointmentsKML } from "./appointments";
 describe("getAppointmentsKML", () => {
   it("renders KML for the retrieved list of appointments", async () => {
     performQuery.mockResolvedValue([
-      { type: "install", date: 946713600000, lat: 91.1442, lng: 133.2211, alt: 300, request_id: 1111 },
-      { type: "install", date: 946704500000, lat: 91.1423, lng: 133.2910, alt: 100, request_id: 2222 },
-      { type: "install", date: 946722200000, lat: 91.1401, lng: 133.2401, alt: 250, request_id: 3333 },
+      {
+        id: 1,
+        type: 'install',
+        date: new Date('2020-12-04T18:00:00.000Z'),
+        notes: null,
+        request_id: 1810,
+        member_id: 1668,
+        building_id: 1672,
+        node_id: null,
+        acuity_id: 472552065,
+        slack_ts: null,
+        address: '123 4th Street',
+        lat: 40.7253199,
+        lng: -73.9403011,
+        alt: 25,
+      },
+      {
+        id: 2,
+        type: 'install',
+        date: new Date('2020-10-01T14:00:00.000Z'),
+        notes: null,
+        request_id: 1811,
+        member_id: 1669,
+        building_id: 1673,
+        node_id: null,
+        acuity_id: 472552066,
+        slack_ts: null,
+        address: '987 6th Ave',
+        lat: 40.7253234,
+        lng: -73.9404138,
+        alt: 21,
+      },
     ]);
 
     expect(await getAppointmentsKML()).toMatchSnapshot();
