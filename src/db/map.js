ca6478649a9e184783f72048284199a3575e5b2c
olivernyc <oliver@olivernyc.net>
2021-01-30 18:18:42 -0500

    Separate authorized map query

--- src/db/map.js
+++ src/db/map.js
@@ -1,63 +1,20 @@
 import { performQuery } from ".";
 
-const getMapQuery = `SELECT 
+const authorizedGetMapQuery = `SELECT 
   json_build_object(
-    'nodes', 
-    (
-      SELECT
-        json_agg(
-          json_build_object(
-            'id', nodes.id,
-            'lat', nodes.lat, 
-            'lng',  nodes.lng, 
-            'status', nodes.status, 
-            'name', nodes.name,
-            'notes', nodes.notes,
-            'devices', (
-              SELECT
-                json_agg(
-                  json_build_object(
-                    'id', devices.id,
-                    'type', device_types,
-                    'lat', devices.lat,
-                    'lng', devices.lng,
-                    'azimuth', devices.azimuth,
-                    'status', devices.status
-                  )
-                )
-              FROM
-                devices
-                LEFT JOIN device_types ON device_types.id = devices.device_type_id
-              WHERE devices.node_id = nodes.id
-            )
-          )
-      )
-      FROM 
-        nodes
-    ), 
-    'requests', 
-    (
-      SELECT 
-        json_agg(
-          json_build_object(
-            'id', requests.id, 
-            'lat', buildings.lat, 
-            'lng', buildings.lng, 
-            'status', requests.status
-          )
-        ) 
-      FROM 
-        requests 
-        JOIN buildings ON buildings.id = requests.building_id
-    ), 
-    'links', 
-    (
-      SELECT 
-        json_agg(
-          json_build_object(
-            'id', links.id,
-            'status', links.status,
-            'devices', (
+    'nodes',
+    COALESCE( 
+      (
+        SELECT
+          json_agg(
+            json_build_object(
+              'id', nodes.id,
+              'lat', nodes.lat, 
+              'lng',  nodes.lng, 
+              'status', nodes.status, 
+              'name', nodes.name,
+              'notes', nodes.notes,
+              'devices', (
                 SELECT
                   json_agg(
                     json_build_object(
@@ -65,22 +22,171 @@ const getMapQuery = `SELECT
                       'type', device_types,
                       'lat', devices.lat,
                       'lng', devices.lng,
-                      'node_id', devices.node_id
+                      'azimuth', devices.azimuth,
+                      'status', devices.status
                     )
                   )
                 FROM
                   devices
                   LEFT JOIN device_types ON device_types.id = devices.device_type_id
-                WHERE devices.id IN(links.device_a_id, links.device_b_id)
+                WHERE devices.node_id = nodes.id
               )
-          )
+            )
         )
-      FROM 
-        links
+        FROM 
+          nodes
+      ),
+      '[]'
+    ),
+    'requests',
+    COALESCE( 
+      (
+        SELECT 
+          json_agg(
+            json_build_object(
+              'id', requests.id, 
+              'lat', buildings.lat, 
+              'lng', buildings.lng, 
+              'status', requests.status
+            )
+          ) 
+        FROM 
+          requests 
+          JOIN buildings ON buildings.id = requests.building_id
+      ),
+      '[]'
+    ), 
+    'links', 
+    COALESCE(
+      (
+        SELECT 
+          json_agg(
+            json_build_object(
+              'id', links.id,
+              'status', links.status,
+              'devices', (
+                  SELECT
+                    json_agg(
+                      json_build_object(
+                        'id', devices.id,
+                        'type', device_types,
+                        'lat', devices.lat,
+                        'lng', devices.lng,
+                        'node_id', devices.node_id
+                      )
+                    )
+                  FROM
+                    devices
+                    LEFT JOIN device_types ON device_types.id = devices.device_type_id
+                  WHERE devices.id IN(links.device_a_id, links.device_b_id)
+                )
+            )
+          )
+        FROM 
+          links
+      ),
+      '[]'
+    ),
+    'appointments',
+    COALESCE(
+      (
+        SELECT
+          json_agg(
+            json_build_object(
+              'id', appointments.id,
+              'type', appointments.type,
+              'lat', buildings.lat,
+              'lng', buildings.lng
+            )
+          )
+          FROM appointments
+          JOIN buildings ON buildings.id = appointments.building_id
+      ),
+      '[]'
     )
   ) as map`;
 
-export async function getMap() {
-  const [map] = await performQuery(getMapQuery);
-  return map.map;
+const getMapQuery = `SELECT 
+  json_build_object(
+    'nodes',
+    COALESCE( 
+      (
+        SELECT
+          json_agg(
+            json_build_object(
+              'id', nodes.id,
+              'lat', nodes.lat, 
+              'lng',  nodes.lng, 
+              'status', nodes.status, 
+              'name', nodes.name,
+              'notes', nodes.notes,
+              'devices', (
+                SELECT
+                  json_agg(
+                    json_build_object(
+                      'id', devices.id,
+                      'type', device_types,
+                      'lat', devices.lat,
+                      'lng', devices.lng,
+                      'azimuth', devices.azimuth,
+                      'status', devices.status
+                    )
+                  )
+                FROM
+                  devices
+                  LEFT JOIN device_types ON device_types.id = devices.device_type_id
+                WHERE devices.node_id = nodes.id
+              )
+            )
+        )
+        FROM 
+          nodes
+      ),
+      '[]'
+    ),
+    'requests',
+    '[]'::json, 
+    'links', 
+    COALESCE(
+      (
+        SELECT 
+          json_agg(
+            json_build_object(
+              'id', links.id,
+              'status', links.status,
+              'devices', (
+                  SELECT
+                    json_agg(
+                      json_build_object(
+                        'id', devices.id,
+                        'type', device_types,
+                        'lat', devices.lat,
+                        'lng', devices.lng,
+                        'node_id', devices.node_id
+                      )
+                    )
+                  FROM
+                    devices
+                    LEFT JOIN device_types ON device_types.id = devices.device_type_id
+                  WHERE devices.id IN(links.device_a_id, links.device_b_id)
+                )
+            )
+          )
+        FROM 
+          links
+      ),
+      '[]'
+    )
+  ) as map`;
+
+export async function getMap(authorized) {
+  let map;
+  if (authorized) {
+    const results = await performQuery(authorizedGetMapQuery);
+    map = results[0].map;
+  } else {
+    const results = await performQuery(getMapQuery);
+    map = results[0].map;
+  }
+  return map;
 }
