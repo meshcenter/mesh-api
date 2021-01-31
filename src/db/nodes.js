19ff6892be6126bbdbd3bd559be7ea98a25ae980
olivernyc <oliver@olivernyc.net>
2021-01-30 19:05:13 -0500

    Clean up query

--- src/db/nodes.js
+++ src/db/nodes.js
@@ -1,16 +1,23 @@
 import { performQuery } from ".";
 
+const nodeDevicesQuery = `SELECT
+  json_agg(json_build_object('id', devices.id, 'type', device_types, 'lat', devices.lat, 'lng', devices.lng, 'alt', devices.alt, 'azimuth', devices.azimuth, 'status', devices.status, 'name', devices.name, 'ssid', devices.ssid, 'notes', devices.notes, 'create_date', devices.create_date, 'abandon_date', devices.abandon_date)) AS devices
+FROM
+  devices
+  LEFT JOIN device_types ON device_types.id IN(devices.device_type_id)
+WHERE
+  devices.node_id = nodes.id`;
+
 const getNodesQuery = `SELECT
-  nodes.*,
+  nodes.id,
+  nodes.lat,
+  nodes.lng,
+  nodes.alt,
+  nodes.status,
+  nodes.name,
+  nodes.notes,
   buildings.address AS building,
-  (
-    SELECT
-      json_agg(json_build_object('id', devices.id, 'type', device_types, 'lat', devices.lat, 'lng', devices.lng, 'alt', devices.alt, 'azimuth', devices.azimuth, 'status', devices.status, 'name', devices.name, 'ssid', devices.ssid, 'notes', devices.notes, 'create_date', devices.create_date, 'abandon_date', devices.abandon_date)) AS devices
-    FROM
-      devices
-    LEFT JOIN device_types ON device_types.id IN(devices.device_type_id)
-  WHERE
-    devices.node_id = nodes.id) AS devices
+  (${nodeDevicesQuery}) AS devices
 FROM
   nodes
   LEFT JOIN buildings ON nodes.building_id = buildings.id
@@ -22,33 +29,47 @@ GROUP BY
 ORDER BY
   nodes.create_date DESC`;
 
-const getNodeQuery = `SELECT
+const authorizedGetNodesQuery = `SELECT
   nodes.*,
-  to_json(buildings) AS building,
+  buildings.address AS building,
+  (${nodeDevicesQuery}) AS devices
+FROM
+  nodes
+  LEFT JOIN buildings ON nodes.building_id = buildings.id
+  LEFT JOIN devices ON nodes.id = devices.node_id
+  LEFT JOIN device_types ON device_types.id IN (devices.device_type_id)
+GROUP BY
+  nodes.id,
+  buildings.id
+ORDER BY
+  nodes.create_date DESC`;
+
+const connectedNodesQuery = `SELECT
+  json_agg(json_build_object('id', nodes.id, 'lat', nodes.lat, 'lng', nodes.lng, 'alt', nodes.alt, 'status', nodes.status, 'name', nodes.name, 'notes', nodes.notes))
+FROM
+  devices devices1
+  JOIN links ON links.device_a_id = devices1.id
+    OR links.device_b_id = devices1.id
+  JOIN devices devices2 ON devices2.id = links.device_b_id
+    OR devices2.id = links.device_a_id
+  JOIN nodes nodes ON nodes.id = devices2.node_id
+WHERE
+  devices1.node_id = $1
+  AND nodes.id != $1
+  AND links.status = 'active'`;
+
+const getNodeQuery = `SELECT
+  nodes.id,
+  nodes.lat,
+  nodes.lng,
+  nodes.alt,
+  nodes.status,
+  nodes.name,
+  nodes.notes,
+  json_build_object('id', buildings.id, 'lat', buildings.lat, 'lng', buildings.lng, 'alt', buildings.alt, 'bin', buildings.bin, 'notes', buildings.notes) AS building,
   COALESCE(json_agg(panoramas ORDER BY panoramas.date DESC) FILTER (WHERE panoramas IS NOT NULL), '[]') AS panoramas,
-  (
-    SELECT
-      json_agg(json_build_object('id', devices.id, 'type', device_types, 'lat', devices.lat, 'lng', devices.lng, 'alt', devices.alt, 'azimuth', devices.azimuth, 'status', devices.status, 'name', devices.name, 'ssid', devices.ssid, 'notes', devices.notes, 'create_date', devices.create_date, 'abandon_date', devices.abandon_date)) AS devices
-    FROM
-      devices
-    LEFT JOIN device_types ON device_types.id IN(devices.device_type_id)
-  WHERE
-    devices.node_id = nodes.id) AS devices,
-  (
-    SELECT json_agg(DISTINCT
-      nodes.*)
-    FROM
-      devices devices1
-      JOIN links ON links.device_a_id = devices1.id
-        OR links.device_b_id = devices1.id
-      JOIN devices devices2 ON devices2.id = links.device_b_id
-        OR devices2.id = links.device_a_id
-      JOIN nodes nodes ON nodes.id = devices2.node_id
-    WHERE
-      devices1.node_id = $1
-      AND nodes.id != $1
-      AND links.status = 'active'
-  ) AS connected_nodes
+  (${nodeDevicesQuery}) AS devices,
+  (${connectedNodesQuery}) AS connected_nodes
 FROM
   nodes
   LEFT JOIN buildings ON nodes.building_id = buildings.id
@@ -60,46 +81,25 @@ GROUP BY
   nodes.id,
   buildings.id`;
 
+const nodeMembersQuery = `SELECT
+  json_agg(json_build_object('id', members.id, 'name', members.name, 'email', members.email, 'phone', members.phone, 'membership_id', memberships.id))
+FROM
+  members
+  LEFT JOIN memberships ON memberships.member_id = members.id
+WHERE
+  memberships.node_id = $1`;
+
 const authorizedGetNodeQuery = `SELECT
   nodes.*,
   to_json(buildings) AS building,
-  json_agg(DISTINCT members_with_membership_id) AS members,
+  (${nodeMembersQuery}) AS members,
   json_agg(DISTINCT requests) AS requests,
   COALESCE(json_agg(panoramas ORDER BY panoramas.date DESC) FILTER (WHERE panoramas IS NOT NULL), '[]') AS panoramas,
-  (
-    SELECT
-      json_agg(json_build_object('id', devices.id, 'type', device_types, 'lat', devices.lat, 'lng', devices.lng, 'alt', devices.alt, 'azimuth', devices.azimuth, 'status', devices.status, 'name', devices.name, 'ssid', devices.ssid, 'notes', devices.notes, 'create_date', devices.create_date, 'abandon_date', devices.abandon_date)) AS devices
-    FROM
-      devices
-    LEFT JOIN device_types ON device_types.id IN(devices.device_type_id)
-  WHERE
-    devices.node_id = nodes.id) AS devices,
-  (
-    SELECT json_agg(DISTINCT
-      nodes.*)
-    FROM
-      devices devices1
-      JOIN links ON links.device_a_id = devices1.id
-        OR links.device_b_id = devices1.id
-      JOIN devices devices2 ON devices2.id = links.device_b_id
-        OR devices2.id = links.device_a_id
-      JOIN nodes nodes ON nodes.id = devices2.node_id
-    WHERE
-      devices1.node_id = $1
-      AND nodes.id != $1
-      AND links.status = 'active'
-  ) AS connected_nodes
+  (${nodeDevicesQuery}) AS devices,
+  (${connectedNodesQuery}) AS connected_nodes
 FROM
   nodes
-  LEFT JOIN buildings ON nodes.building_id = buildings.id
-  LEFT JOIN memberships ON memberships.node_id = nodes.id
-  LEFT JOIN (
-    SELECT
-      members.*,
-      memberships.id AS membership_id
-    FROM members
-    LEFT JOIN memberships ON memberships.member_id = members.id
-  ) AS members_with_membership_id ON memberships.id = members_with_membership_id.membership_id
+  LEFT JOIN buildings ON buildings.id = nodes.building_id
   LEFT JOIN requests ON requests.building_id = buildings.id
   LEFT JOIN panoramas ON panoramas.request_id = requests.id
 WHERE
