6ad0b62301afc338004f846b8f48949e918052a4
Brian <bh@usa.net>
2021-02-02 18:59:30 -0500

    Vernon (#91)
    * swapping potential node YWCA for Vernon
    
    * moved vernon comment

--- src/db/los.js
+++ src/db/los.js
@@ -52,6 +52,7 @@ GROUP BY
   buildings.id`;
 
 // Get hardcoded requests and requests with scheduled appointments
+// added Vernon 5916 as potential
 const getRequestsQuery = `SELECT
   requests.id,
   requests.status,
@@ -66,7 +67,7 @@ FROM
   JOIN buildings ON requests.building_id = buildings.id
   LEFT JOIN appointments ON appointments.request_id = requests.id
 WHERE
-  requests.id IN(3946)
+  requests.id IN(5916)
   OR(
     SELECT
       id FROM appointments
