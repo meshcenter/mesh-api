78d7d50479bc3dda980c723c4172069dcd0acbc8
olivernyc <oliver@olivernyc.net>
2021-02-14 00:19:22 -0500

    Format

--- src/db/links.js
+++ src/db/links.js
@@ -1,7 +1,7 @@
 import { performQuery } from ".";
 
 export async function getLinks() {
-	return performQuery(`SELECT
+  return performQuery(`SELECT
 	links.*,
 	json_agg(nodes) as nodes,
 	json_agg(json_build_object('id', devices.id, 'type', device_types, 'lat', devices.lat, 'lng', devices.lng, 'alt', devices.alt, 'azimuth', devices.azimuth, 'status', devices.status, 'name', devices.name, 'ssid', devices.ssid, 'notes', devices.notes, 'create_date', devices.create_date, 'abandon_date', devices.abandon_date)) AS devices
@@ -15,8 +15,8 @@ FROM
 }
 
 export async function getLink(id) {
-	const link = await performQuery(
-		`SELECT
+  const link = await performQuery(
+    `SELECT
 	links.*,
 	json_agg(nodes) as nodes,
 	json_agg(json_build_object('id', devices.id, 'type', device_types, 'lat', devices.lat, 'lng', devices.lng, 'alt', devices.alt, 'azimuth', devices.azimuth, 'status', devices.status, 'name', devices.name, 'ssid', devices.ssid, 'notes', devices.notes, 'create_date', devices.create_date, 'abandon_date', devices.abandon_date)) AS devices
@@ -29,23 +29,23 @@ WHERE
   links.id = $1
 GROUP BY
 	links.id`,
-		[id]
-	);
-	if (!link) throw new Error("Not found");
-	return link;
+    [id]
+  );
+  if (!link) throw new Error("Not found");
+  return link;
 }
 
 export async function createLink({ device_a_id, device_b_id }) {
-	const [
-		newLink,
-	] = await performQuery(
-		`INSERT INTO links (device_a_id, device_b_id, status, create_date) VALUES($1, $2, $3, $4) RETURNING *`,
-		[device_a_id, device_b_id, "active", new Date()]
-	);
-	return getLink(newLink.id);
+  const [
+    newLink,
+  ] = await performQuery(
+    `INSERT INTO links (device_a_id, device_b_id, status, create_date) VALUES($1, $2, $3, $4) RETURNING *`,
+    [device_a_id, device_b_id, "active", new Date()]
+  );
+  return getLink(newLink.id);
 }
 
 export async function deleteLink({ id }) {
-	await getLink(id);
-	return performQuery(`DELETE FROM links WHERE id = $1 RETURNING *`, [id]);
+  await getLink(id);
+  return performQuery(`DELETE FROM links WHERE id = $1 RETURNING *`, [id]);
 }
